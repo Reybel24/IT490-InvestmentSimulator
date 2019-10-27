fbfb5f7ec54a5295795ae2d8875441bd81bbc3c1
Tricia Pardave <tmp34@njit.edu>
2019-10-27 16:40:40 -0400

    db design created

--- dump.sql
+++ dump.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `trialrun2`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `trialrun2` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `trialrun2`;
-
---
 -- Table structure for table `accounts`
 --
 
@@ -34,6 +26,8 @@ CREATE TABLE `accounts` (
   `userid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -44,8 +38,89 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,'user1','password'),(2,'user2','password');
+INSERT INTO `accounts` VALUES (1,'user1','password','Pablo','Picasso'),(2,'user2','password','Joel','Bloom');
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `currency_trackers`
+--
+
+DROP TABLE IF EXISTS `currency_trackers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `currency_trackers` (
+  `userid` int(11) NOT NULL,
+  `tracker_id` int(11) NOT NULL AUTO_INCREMENT,
+  `currency_tracked` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`tracker_id`),
+  KEY `fk_accounts_currency_trackers` (`userid`),
+  CONSTRAINT `fk_accounts_currency_trackers` FOREIGN KEY (`userid`) REFERENCES `accounts` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `currency_trackers`
+--
+
+LOCK TABLES `currency_trackers` WRITE;
+/*!40000 ALTER TABLE `currency_trackers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `currency_trackers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `investments`
+--
+
+DROP TABLE IF EXISTS `investments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `investments` (
+  `investment_id` int(11) NOT NULL,
+  `portfolio_id` int(11) NOT NULL,
+  `base_currency` varchar(255) DEFAULT NULL,
+  `target_currency` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`investment_id`),
+  KEY `fk_portfolio_investments` (`portfolio_id`),
+  CONSTRAINT `fk_portfolio_investments` FOREIGN KEY (`portfolio_id`) REFERENCES `portfolio` (`portfolio_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `investments`
+--
+
+LOCK TABLES `investments` WRITE;
+/*!40000 ALTER TABLE `investments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `investments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portfolio`
+--
+
+DROP TABLE IF EXISTS `portfolio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portfolio` (
+  `userid` int(11) NOT NULL,
+  `portfolio_id` int(11) NOT NULL AUTO_INCREMENT,
+  `available_balance` int(11) DEFAULT NULL,
+  `portfolio_value` int(11) DEFAULT NULL,
+  PRIMARY KEY (`portfolio_id`),
+  KEY `fk_accounts_portfolio` (`userid`),
+  CONSTRAINT `fk_accounts_portfolio` FOREIGN KEY (`userid`) REFERENCES `accounts` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+LOCK TABLES `portfolio` WRITE;
+/*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
+INSERT INTO `portfolio` VALUES (1,1,500,0),(2,2,500,0);
+/*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-24 12:20:25
+-- Dump completed on 2019-10-27 16:39:02
