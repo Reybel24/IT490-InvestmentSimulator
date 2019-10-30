d00411e0acfe1d14d28244d421f9f8e4c6afb6fd
Tricia Pardave <tmp34@njit.edu>
2019-10-30 18:32:51 -0400

    adding supporting files

---
+++ logError.php
@@ -0,0 +1,11 @@
+<?php
+
+function writeLogMessage($time, $machine_name, $error_message){
+  $logfile = fopen("funwitherrors.txt", "w") or die("unable to open file!");
+  $txt = $time." ".$machine_name." ".$error_message;
+  fwrite($logfile, $txt)
+  fclose($logfile);
+}
+
+
+?>
---
+++ timetest.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php
+<?php
+$t = time();
+echo(date("m-d-Y h:i:s", $t)."\n");
+?>
