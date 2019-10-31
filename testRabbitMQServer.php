42d2ea55a8233152a588677e9b3612de0dff5a11
Tricia <triciapardave@gmail.com>
2019-10-30 22:07:20 -0400

    dls was working and now its not?

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -38,7 +38,7 @@ function requestProcessor($request)
     case "validate_session":
 			//return doValidate($request['sessionId']);
 		case "error_log":
-			echo "sending error from ".$request['machine_name'];
+			echo "receiving error from ".$request['machine_name'];
 			$returnCode = writeLogMessage($request['time'], $request['machine_name'], $request['error_message']);
 				//return doValidate($request['sessionId']);
   }
