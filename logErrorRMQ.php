20bc97c51e47284b56bf3f5397f2297f02e4b40d
Tricia Pardave <tmp34@njit.edu>
2019-11-05 18:50:09 -0500

    filename clarification

--- logErrorRMQ.php
+++ logErrorRMQ.php
@@ -4,6 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+$errorLog = $argv[1];
 
 function logError($errorLog){
   $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
