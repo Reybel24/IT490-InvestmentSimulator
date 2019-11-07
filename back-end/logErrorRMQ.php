5cd80d9cde2e190190d097c5477c8acd8ae0066e
Tricia Pardave <tmp34@njit.edu>
2019-11-07 12:22:07 -0500

    stress

---
+++ back-end/logErrorRMQ.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+function logError($errorLog){
+  $client = new rabbitMQClient("loggerRMQ.ini","testServer");
+  $machine_info = date("m-d-Y h:i:s", time())." ".gethostname()." ".$errorLog.PHP_EOL;
+
+  $response = $client->publish($machine_info);
+
+}
+?>
